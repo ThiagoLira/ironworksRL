58eeedb8eea8a13ca281604e8c1d626a71d11bc9
Thiago Lira <thlira15@gmail.com>
2021-12-16 16:30:18 -0300

    torch and build script

---
+++ install_lib_torch.sh
@@ -0,0 +1,2 @@
+wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+unzip libtorch-shared-with-deps-latest.zip
---
+++ ironworks_RL/build/build.sh
@@ -0,0 +1,2 @@
+cmake -DCMAKE_PREFIX_PATH=$PWD/../../libtorch ..
+cmake --build . --config Release
